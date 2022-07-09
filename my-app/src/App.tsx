f563d3dcc7b08d7dec5f3c5368ed7508f4cd73be
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-09 10:06:07 +0530

    Added initilization of the socket io-client in the frontend

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { io } from 'socket.io-client';
 function App() {
+  React.useEffect(function () {
+    const socket = io("http://localhost:4000/");
+    socket.on("connect", () => {
+      console.log(socket.id);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
