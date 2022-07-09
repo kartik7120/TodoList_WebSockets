0fbcd8cddb49d6e48bb72514e3b52e5a5dda3c61
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-09 19:26:25 +0530

    Made the form stateful and rendering the different todo list

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,18 @@
+// import React, { forwardRef, useRef } from 'react';
 import React from 'react';
 import './App.css';
 import ToDoForm from './components/todoForm';
 import "./style.css"
 import { io } from 'socket.io-client';
-
+import TodoTask from './components/todoTask';
+// import TodoTask from "./components/todoTask";
+// type todoType = null | string[];
 interface responseObj {
   res: string
 }
 function App() {
+  const [todoState, setTodoState] = React.useState([] as string[]);
+
   React.useEffect(function () {
     const socket = io("http://localhost:4000/");
     socket.on("connect", () => {
@@ -20,7 +25,12 @@ function App() {
   return (
     <div className="App">
       <h1 className='todo-header'>Todos</h1>
-      <ToDoForm />
+      <ToDoForm settodoList={setTodoState} />
+      <div className='todoList'>
+        {todoState.map((ele) => {
+          return <TodoTask text={ele} key={ele + (Math.random() * 20)} />
+        })}
+      </div>
     </div>
   );
 }
--- my-app/src/components/todoForm.tsx
+++ my-app/src/components/todoForm.tsx
@@ -1,8 +1,25 @@
-function ToDoForm() {
-    return <form action="" className="todo-form">
+import React from "react";
+interface propInterface {
+    settodoList: React.Dispatch<React.SetStateAction<string[]>>
+}
+function ToDoForm(props: propInterface) {
+    const [formState, setFormState] = React.useState("");
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const val = e.target.value;
+        setFormState(val);
+    }
+    function submitFunction(e: React.FormEvent): void {
+        e.preventDefault();
+        const text: string = formState;
+        setFormState("");
+        props.settodoList(function (oldState) {
+            return [...oldState, text];
+        })
+    }
+    return <form action="" className="todo-form" onSubmit={submitFunction}>
         <label htmlFor="todo" className="todo-label">Enter Todo Task</label>
         <div className="input-wrapper">
-            <input type="text" name="todoTask" id="todo" className="input-todo" required maxLength={50} />
+            <input type="text" name="todoTask" id="todo" className="input-todo" required maxLength={50} value={formState} onChange={handleChange} />
             <button type="submit" className="submit-button">✅</button>
         </div>
     </form>
